cbe6d5583dcf25a8b777a3c9f696cf95a21d6ca8
mveynberg <mv@cj264.ru>
2020-03-26 01:24:56 +0500

    Draw and replace xhdpi launcher icon; Show it on the initial screen

--- src/meta_wallet_activity.rb
+++ src/meta_wallet_activity.rb
@@ -1,9 +1,7 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
 
-ruboto_import_widgets :Button, :LinearLayout, :TextView
-
-# http://xkcd.com/378/
+ruboto_import_widgets :ImageView, :Button, :LinearLayout, :TextView
 
 class MetaWalletActivity
   def onCreate(bundle)
@@ -11,14 +9,24 @@ class MetaWalletActivity
     set_title 'MetaWallet Ruboto'
 
     self.content_view =
-        linear_layout :orientation => :vertical do
-          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, 
-                                 :layout => {:width => :match_parent},
-                                 :gravity => :center, :text_size => 48.0
-          button :text => 'M-x butterfly', 
-                 :layout => {:width => :match_parent},
-                 :id => 43, :on_click_listener => proc { butterfly }
-        end
+      linear_layout :orientation => :vertical do
+        image_view :image_resource => $package::R::drawable::ic_launcher,
+                   :scale_type => ImageView::ScaleType::FIT_CENTER,
+                   :layout => {
+                     :weight= => 1,
+                     :height= => :fill_parent,
+                     :width= => :fill_parent
+                   }
+        @text_view = text_view :text => 'You have not got any wallets yet.',
+                               :id => 42,
+                               :layout => {:width => :match_parent},
+                               :gravity => :center,
+                               :text_size => 24.0
+        button :text => 'Create Wallet',
+               :layout => {:width => :match_parent},
+               :id => 43,
+               :on_click_listener => proc { butterfly }
+      end
   rescue Exception
     puts "Exception creating activity: #{$!}"
     puts $!.backtrace.join("\n")
